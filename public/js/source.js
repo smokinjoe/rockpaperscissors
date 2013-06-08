40e7e08f9102dd55377c44053a69bee6ad0087e2
Joe Ekiert <joe.ekiert@gmail.com>
2013-06-08 02:02:19 -0400

    new method of storing the action history, hoping it'll make for easier access later, needed to be able to distinguish between opp1 and opp2

--- public/js/source.js
+++ public/js/source.js
@@ -16,70 +16,89 @@ $("#roll-all-at-once").click( function () {
 
   function doBattle (_opp1, _opp2) {
     var _actions = ['rock','paper','scissors'];
-    var record = {
-      'opp1' : {
-        'name' : _opp1.name,
-        'action' : _actions[Math.floor(Math.random() * _actions.length)]
-      },
-      'opp2' : {
-        'name' : _opp2.name,
-        'action' : _actions[Math.floor(Math.random() * _actions.length)]
-      }
+    var record = {};
+    record[_opp1.name] = {
+      'name' : _opp1.name,
+      'action' : _actions[Math.floor(Math.random() * _actions.length)],
+      'result' : ''
+    };
+    record[_opp2.name] = {
+      'name' : _opp2.name,
+      'action' : _actions[Math.floor(Math.random() * _actions.length)],
+      'result' : ''
     };
 
-    if (record.opp1.action === "rock") {
-      switch (record.opp2.action) {
+    if (record[_opp1.name].action === "rock") {
+      switch (record[_opp2.name].action) {
         case "rock":
           _opp1.draws += 1;
           _opp2.draws += 1;
+          record[_opp1.name].result = 'draw';
+          record[_opp2.name].result = 'draw';
           doBattle(_opp1, _opp2);
           break;
         case "paper":
           _opp1.losses += 1;
           _opp2.wins += 1;
+          record[_opp1.name].result = 'loss';
+          record[_opp2.name].result = 'win';
           break;
         case "scissors":
           _opp1.wins += 1;
           _opp2.losses += 1;
+          record[_opp1.name].result = 'win';
+          record[_opp2.name].result = 'loss';
           break;
         default:
           alert("Something has gone wrong");
           break;
       }
     }
-    else if (record.opp1.action === "paper") {
-      switch (record.opp2.action) {
+    else if (record[_opp1.name].action === "paper") {
+      switch (record[_opp2.name].action) {
         case "rock":
           _opp1.wins += 1;
           _opp2.losses += 1;
+          record[_opp1.name].result = 'win';
+          record[_opp2.name].result = 'loss';
           break;
         case "paper":
           _opp1.draws += 1;
           _opp2.draws += 1;
+          record[_opp1.name].result = 'draw';
+          record[_opp2.name].result = 'draw';
           doBattle(_opp1, _opp2);
           break;
         case "scissors":
           _opp1.losses += 1;
           _opp2.wins += 1;
+          record[_opp1.name].result = 'loss';
+          record[_opp2.name].result = 'win';
           break;
         default:
           alert("Something has gone wrong");
           break;
       }
     }
-    else if (record.opp1.action === "scissors") {
-      switch (record.opp2.action) {
+    else if (record[_opp1.name].action === "scissors") {
+      switch (record[_opp2.name].action) {
         case "rock":
           _opp1.losses += 1;
           _opp2.wins += 1;
+          record[_opp1.name].result = 'loss';
+          record[_opp2.name].result = 'win';
           break;
         case "paper":
           _opp1.wins += 1;
           _opp2.losses += 1;
+          record[_opp1.name].result = 'win';
+          record[_opp2.name].result = 'loss';
           break;
         case "scissors":
           _opp1.draws += 1;
           _opp2.draws += 1;
+          record[_opp1.name].result = 'draw';
+          record[_opp2.name].result = 'draw';
           doBattle(_opp1, _opp2);
           break;
         default:
@@ -91,8 +110,33 @@ $("#roll-all-at-once").click( function () {
       alert('something has gone wrong');
     }
 
-    _opp1.action_history.push(record);
-    _opp2.action_history.push(record);
+    var _opp1_record = {
+      'hero' : {
+        'action' : record[_opp1.name].action,
+        'name' : _opp1.name
+      },
+      'opponent' : {
+        'action' : record[_opp2.name].action,
+        'name' : _opp2.name
+      },
+      'result' : record[_opp1.name].result
+    };
+
+    var _opp2_record = {
+      'hero' : {
+        'action' : record[_opp2.name].action,
+        'name' : _opp2.name
+      },
+      'opponent' : {
+        'action' : record[_opp1.name].action,
+        'name' : _opp1.name
+      },
+      'result' : record[_opp2.name].result
+    };
+
+    _opp1.action_history.push(_opp1_record);
+    _opp2.action_history.push(_opp2_record);
+
     _opp1.games += 1;
     _opp2.games += 1;
   }
@@ -135,14 +179,15 @@ $("#roll-all-at-once").click( function () {
 
   for (var _i = 0; _i < _stuyguys.length; _i += 1) {
     var _return_string = '<tr>'
-                + '<td>' + _stuyguys[_i].name + '</td>'
+                + '<td class="popover-me">' + _stuyguys[_i].name + '</td>'
                 + '<td>' + _stuyguys[_i].wins + '</td>'
                 + '<td>' + _stuyguys[_i].losses + '</td>'
                 + '<td>' + _stuyguys[_i].draws + '</td>'
                 + '<td>' + _stuyguys[_i].games + '</td>'
               + '</tr>';
     $("#results-body").append(_return_string);
+
   }
 
-  //console.log(_stuyguys);
+  console.log(_stuyguys);
 });
